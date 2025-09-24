c6c80db891adff89418faf4de97a7ab521718cd2
adeilsoncardoso <adeilsoncardoso00@gmail.com>
2025-09-24 20:30:07 -0300

    Add files via upload

---
+++ #atividade 2.py
@@ -0,0 +1,22 @@
+#atividade 2 
+def Selection-Sort (lista)
+#criar uma lista usando o algoritmo selection-sort
+#ordene a lista [64, 25, 12, 22, 11]
+n = len (lista)
+# passar por toda a lista selecionando o numero da lista
+indice_menor = i
+if (lista) [j]
+for in range (i + 1, n)
+print(lista)
+
+
+[64, 25, 12, 22, 11]
+
+
+
+
+
+
+
+
+
